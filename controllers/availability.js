0cdc7cdc11a9ceac738af9133b31b89a29c542c1
A. Drake <a.l.drake713@gmail.com>
2015-03-30 12:36:15 -0400

    added momentjs and updated some error handling on meetings controller. meetings returns times in UTC

--- controllers/availability.js
+++ controllers/availability.js
@@ -8,14 +8,24 @@ var	Meeting = require('../models/meeting');
 var User = require('../models/user');
 var Avail = require('../models/availability');
 var _ = require('lodash');
+var moment = require('moment-timezone');
 
 exports.addAvail = function(req, res) {
 	// create a new object
 	var avail = new Avail({});
+
 	// create Date objects from the input strings
-	avail.start = new Date(req.body.start);
-	avail.end = new Date(req.body.end);
+	/*avail.start = new Date(req.body.start);
+	avail.end = new Date(req.body.end);*/
+
+	var startTime = moment(req.body.start, "YYYY-MM-DD HH:mm");
+	var endTime = moment(req.body.end, "YYYY-MM-DD HH:mm");
+
+	avail.start = startTime;
+	avail.end = endTime;
+
 	avail.username = req.user._id;
+
 	// add it to a meeting
 	Meeting.findById(req.params.meeting_id, function(err, meeting) {
 		if (err) {
@@ -24,7 +34,7 @@ exports.addAvail = function(req, res) {
 		if (!meeting) {
 			res.json({message: "Sorry, this meeting doesn't exist."});
 		} else {
-			meeting.avail.push(avail);
+			meeting.availability.push(avail);
 			meeting.save(function(err) {
 				if (err) {
 					res.send(400);
