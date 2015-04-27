3e07ac7ab194a36b04abd5a83996173b83c02603
A. Drake <a.l.drake713@gmail.com>
2015-04-27 11:14:56 -0400

    reverted angular changes

--- public/js/users/users.js
+++ public/js/users/users.js
@@ -1,10 +1,6 @@
 angular.module('TimeCoordinator.Users', [])
 .controller('UserController', ['$scope', 'ActiveUser', function($scope, ActiveUser) {
   var activeuser = window.user;
-  $scope.user = ActiveUser.get({user_id: activeuser})
-}])
-.controller('SignupController', ['$scope', function($scope) {
-	// form fields
-	$scope.user;
+  $scope.user = ActiveUser.get({user_id: activeuser});
 }]);
 
